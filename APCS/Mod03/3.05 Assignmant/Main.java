94d034edb01d24936190d7e0523bc4f63243ce8d
Shawn Velsor <hiddenworlds225@gmail.com>
2020-10-22 09:51:21 -0400

    10/22/2020

--- APCS/Mod03/3.05 Assignmant/Main.java
+++ APCS/Mod03/3.05 Assignmant/Main.java
@@ -1,8 +1,11 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 /*
     @Author: Shawn Velsor
     Date: 10/20/2020
+
+    Purpose: To get BMI
 
 
 */
@@ -13,18 +16,28 @@ public class Main
     public static void main(String[] args)
     {
         Scanner scan = new Scanner(System.in);
+        int[] heightImperial;
+        float metersTall;
         String firstName;
         String lastName;
 
-
-
         
-        
-        //System.out.println("Hi");
-        System.err.println("Hi");
-
+        System.out.print("Welcome, please enter your first name: ");
         firstName = scan.nextLine();
+        System.out.print("Enter your last name: ");
         lastName = scan.nextLine();
+
+        System.out.print("Please enter your height ( feet,inches )");
+        String heightRaw = scan.next();
+        String[] heightRawArr;
+        heightRaw = heightRaw.replaceAll("\\s", "");
+        
+        //Yhea, this is a very complicated line, but it works.
+        heightImperial = Arrays.stream(heightRaw.split(",")).mapToInt(Integer::parseInt).toArray(); 
+
+         heightImperial[0]
+
+        //Quick Maths
 
         System.out.println(firstName);
         System.out.println(lastName);
