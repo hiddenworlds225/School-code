a8834c075cdf77e77bf5c720929d2190a9da1e2f
Shawn Velsor <hiddenworlds225@gmail.com>
2021-02-05 19:10:33 -0500

    2/5/2021

--- APCS/Mod06/CheeseCake/CheeseCake.java
+++ APCS/Mod06/CheeseCake/CheeseCake.java
@@ -1,10 +1,10 @@
 package CheeseCake;
-
+ 
 public class CheeseCake {
     //Pre-Set variables for calculations, set here for easy modification.
     private int servings = 16;
     private int ounceCreamCheese = 32;
-    private double cupSugar = 1/3;
+    private double cupSugar = 1.0/3;
     private int tspVanila = 1;
 
     //user driven variables
@@ -12,12 +12,23 @@ public class CheeseCake {
     private String flavor;
 
     //Final results, can be requested by the user in the code
-    public int totalServings; 
-    public int totalCreamCheese;
-    public double totalCupSugar;
-    public int totalTspVanilla;
+    private int totalServings; 
+    private int totalCreamCheese;
+    private double totalCupSugar;
+    private int totalTspVanilla;
 
+    public CheeseCake(String flavor, int quantity){
+        this.quantity = quantity;
+        this.flavor = flavor;
+    }
 
+    //Just a quck way without having to do it bit by bit.
+    public void calcAllData(){
+        calcCreamCheese();
+        calcSugar();
+        calcTotalServings();
+        calcVanila();
+    }
 
 
     public void calcTotalServings(){
@@ -31,5 +42,27 @@ public class CheeseCake {
     }
     public void calcVanila(){
         totalTspVanilla = quantity * tspVanila;
+    }
+
+    //Use these to get variables from the item in question.
+    public int totalServings(){
+        return totalServings;
+    }
+
+    public int totalCreamCheese(){
+        return totalCreamCheese;
+    }
+
+    public double totalSugar(){
+        return totalCupSugar;
+    }
+
+    public int totalVanilla(){
+        return totalTspVanilla;
+    }
+
+    public String toString(int index){
+        return String.format("|%4d   |%5d     | %-11s   |%8d oz   |%7d   | %2.2f  cups|   %d tsps  |", 
+        index, quantity, flavor, totalCreamCheese, totalServings, totalCupSugar, totalTspVanilla);
     }
 }
--- APCS/Mod06/CheeseCake/CheeseCakeTester.java
+++ APCS/Mod06/CheeseCake/CheeseCakeTester.java
@@ -1,7 +1,14 @@
 package CheeseCake;
 
 /*
+    Was going to say half of it was made by me, but its completely similar to the templates .-.
 
+    7.02 ArrayLists II
+
+    Expanding knowledge and use of ArrayLists
+
+    @Shawn Velsor
+    @2/4/2021
 
 */
 
@@ -11,6 +18,32 @@ import java.util.ArrayList;
 
 public class CheeseCakeTester {
     public static void main(String[] args) {
-        ArrayList<CheeseCake> cheeseCakes = new ArrayList<CheeseCake>();
+        ArrayList<CheeseCake> cheeseCakes = new ArrayList<>();
+
+        cheeseCakes.add(new CheeseCake("Blueberry", 5));
+        cheeseCakes.add(new CheeseCake("Strawberry", 6));
+        cheeseCakes.add(new CheeseCake("Cherry", 4));
+        cheeseCakes.add(new CheeseCake("Jalanpeno", 1));
+        cheeseCakes.add(new CheeseCake("Dill Pickle", 2));
+        cheeseCakes.add(new CheeseCake("Fig", 3));
+
+        //printing the format of the table
+        System.out.println("|       Cheese Cake Data           |             Ingredient Calculations             |");
+        System.out.println("| Index | Quantity |  Flavor       | Cream Cheese | Servings |   Sugar   |  Vanilla  |");
+        System.out.println("|-------|----------|---------------|--------------|----------|-----------|-----------|");
+
+         //using a for loop to print out the objects' data
+         for(int i = 0; i < cheeseCakes.size(); i++)
+         {
+
+            //Use for smaller code, does the same thing
+            //cheeseCakes.get(i).calcAllData();
+
+            cheeseCakes.get(i).calcCreamCheese();
+            cheeseCakes.get(i).calcTotalServings();
+            cheeseCakes.get(i).calcSugar();
+            cheeseCakes.get(i).calcVanila();
+            System.out.printf("%s%n", cheeseCakes.get(i).toString(i));
+         }
     }
 }
